4b4447ca8aad502cc5a4aacce5ac16b6bb0f3fb1
nentu <zajtsev.artjom@samlit.net>
2022-12-11 14:34:15 +0300

    replace getClass with instanceof

---
+++ src/story/Viewer.java
@@ -0,0 +1,8 @@
+package story;
+
+public class Viewer {
+    public String getClassName(){return "Viewer";}
+    public static void view (String string){
+        System.out.print(string);
+    }
+}
